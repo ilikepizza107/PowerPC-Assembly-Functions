94ea37dff9fdb6875a716411d91cb8256f8a5b1a
QuickLava <fastmovingvolcanomatter@gmail.com>
2023-06-15 02:06:01 -0700

    One More Minor Theme Code Optimization
    Rearranged operations to more efficiently use just reg1 and reg2, eliminating the need to backup and retrieve the string reg's value from LR (saves 2 instructions lmao).

--- PowerPC Assembly Functions/_ThemeChange.cpp
+++ PowerPC Assembly Functions/_ThemeChange.cpp
@@ -25,31 +25,23 @@ void themeChangerBody(const std::string menuDirectory, themeConstants::themePath
 		}
 
 		// Actual Body
-		// Write and skip over the prefixes string...
-		BL(1 + (prefixesString.size() / 4));
-		WriteTextToFile(prefixesString);
-		// ... then put its address in reg1, which we'll use in just a moment.
-		MFLR(reg1);
-
-		// This is fucked up, but we're using LR as an extra register lmfao
-		// Backup the string reg's address in LR
-		MTLR(stringReg);
-
 		// Get the selected theme from the Theme Setting Line
 		ADDIS(reg2, 0, THEME_SETTING_INDEX >> 0x10);
 		LWZ(reg2, reg2, (THEME_SETTING_INDEX & 0xFFFF) + Line::VALUE);
+		// Use reg1 to calc index into what will be our prefix list; first set it to 4...
+		SetRegister(reg1, 0x04);
+		// ... then multiply it by the desired Theme ID to index into the prefix list. reg2 is now our offset!
+		MULLW(reg2, reg2, reg1);
 
-		// Borrow the string register, set it to 4...
-		SetRegister(stringReg, 0x04);
-		// ... then multiply it by the desired Theme ID to index into the prefix list.
-		MULLW(reg2, reg2, stringReg);
-		// Then load the relevant prefix string using the calculated offset. reg1 is now our prefix!
+		// Write and skip over the prefixes string...
+		BL(1 + (prefixesString.size() / 4));
+		WriteTextToFile(prefixesString);
+		// ... then put its address in reg1, which we'll use to...
+		MFLR(reg1);
+		// ... load the relevant prefix string using the calculated offset. reg1 is now our prefix!
 		LWZX(reg1, reg1, reg2);
 
-		// Retrieve string reg value from LR
-		MFLR(stringReg);
-
-		// Now store the prefix in the correct spot, and we're done!
+		// Finally store the prefix in the correct spot, and we're done!
 		STW(reg1, stringReg, menuDirectory.size() - 1);
 	}
 }
