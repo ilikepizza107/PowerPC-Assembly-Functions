2b7b6ff9d84a06fdde98437ea093ce2e47d8ecc3
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-03-15 17:52:14 -0700

    Updated Version Number

--- PowerPC Assembly Functions/_AdditionalCode.cpp
+++ PowerPC Assembly Functions/_AdditionalCode.cpp
@@ -3,7 +3,7 @@
 
 namespace lava
 {
-	const std::string version = "v1.4.0";
+	const std::string version = "v1.4.5";
 	int CMNUCopyOverride = INT_MAX;
 	int ASMCopyOverride = INT_MAX;
 	int GCTBuildOverride = INT_MAX;
