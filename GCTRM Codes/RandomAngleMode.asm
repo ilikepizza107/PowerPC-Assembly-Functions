23be9e8cb3eac5686de53f1feae3d590f73e21ef
QuickLava <fastmovingvolcanomatter@gmail.com>
2025-03-13 17:41:08 -0700

    Updated External GCTRM Codes to Modern Versions
    I've just kinda ignored updating these all this time; will endeavor to stay on top of them going forwards though!

--- GCTRM Codes/RandomAngleMode.asm
+++ GCTRM Codes/RandomAngleMode.asm
@@ -1,35 +1,61 @@
-###########################################################
-Random Angle Mode 1.2 [PyotrLuzhin, Eon]
-###########################################################
-#Code Menu Variant by Desi
-.alias CodeMenuStart = 0x804E
-.alias CodeMenuHeader = 0x02BC       #Offset of word containing location of the Random Angle Mode Toggle. Source is compiled with headers for this.
-HOOK @ $80767adc
+#####################################################################
+Random Angle Mode 1.3.0 [PyotrLuzhin, DesiacX, Eon, QuickLava]
+# v1.3.0 - Rolls in old Full Random Mode as another Code Menu Setting
+#####################################################################
+.alias CM_HeaderStart = 0x804E
+.alias CM_RandAngleLOCOff = 0x02BC  # Code Menu Header offset to Random Angle Mode Line Address.
+
+HOOK @ $807449E0    # 0xD8 bytes into symbol "set/[soCollisionAttackModuleImpl]/so_collision_attack_mod" @ 0x80744908
 {
-  lwz r0, 0x14(r3)
-  stwu r1, -0x10(r1)
-loc_0x0:
-  stw r3, 0x08(r1)
-  stw r4, 0x0C(r1)
-  lis r4, CodeMenuStart
-  ori r4, r4, CodeMenuHeader    #Load Code Menu Header
-  lwz r4, 0 (r4)
-  lbz r4, 0xB(r4)
-  cmpwi r4, 0x1
-  bne- loc_0x5C
+  lwz r6, 0x14(r30)
 
-loc_0x28:
-#randi
-  li r3, 360
-  lis r12, 0x8003
-  ori r12, r12, 0xfc7c
-  mtctr r12
-  bctrl
-  addi r0, r3, 1
+  lis r4, CM_HeaderStart                       # \
+  lwz r4, CM_RandAngleLOCOff(r4)               # / Get address of Random Angle Line
+  lwz r4, 0x8(r4)                              # Get the current value of the line.
+  cmplwi r4, 0x1                               # \ 
+  blt+ exit                                    # / If value < 1 (ie. 0), Random Angle Mode is off, skip to exit!
+  bgt getMatchRandomSeed                       # If value > 1 (ie. 2), Random Angle Mode is set to Static, jump to that.
+  
+getFrameRandomSeed:                            # Otherwise, Random Angle Mode is 1 (True Random)...
+  lwz r4, -0x4364(r13)                         # ... so grab current frame's random seed!
+  b adjustSeed                                 # Skip grabbing Match Random Seed.
+  
+getMatchRandomSeed:
+  lis r4, 0x8071                               # \
+  subi r4, r4, 0x7340                          # |
+  addi r4, r4, 0x10                            # | Get FightSeed
+  lwz r4, 0x54(r4)                             # /
+  
+adjustSeed:                                    # Now adjust seed based on current fighter state.
+  lwz r6, 0x28(r28)                            # Get moduleAccessor   
 
-loc_0x5C:
+  lwz r5, 0x8(r6)                              # Get Fighter* from Accessor...
+  lwz r5, 0x110(r5)                            # ... and FighterKind from that.
+  add r4, r4, r5                               # Add FighterKind to seed.
 
-  lwz r3, 0x08(r1)
-  lwz r4, 0x0C(r1)
-  addi r1, r1, 0x10
+  lwz r5, 0xD8(r6)                             # Get ModuleEnum from Accessor.
+  lwz r5, 0x8(r5)                              # Get MotionModule* from Enum...
+  lhz r5, 0x5A(r5)                             # ... and lower half of MotionKind from that.
+  mulli r5, r5, 100                            # Multipliy by 100...
+  add r4, r4, r5                               # ... then add to seed.
+
+  lwz r6, 0x18(r30)                            # Grab Knockback Growth from CollisionAttackData struct
+  rlwinm r5, r6, 12, 0, 19                     # Multiply by 0x1000...
+  add r4, r4, r5                               # ... then add to seed.
+
+  # just write my own randi that doesnt have a state coz this is its own call and thats easier than saving and restoring the games seed
+  # r4 is starting seed
+  lis r6, 0x41C6                               # \
+  addi r6, r6, 0x4E6D                          # / Set multiplication factor...
+  mullw r6, r4, r6                             # ... and multiply it into seed.
+  li r5, 360                                   # \
+  divwu r5, r6, r5                             # | 
+  mulli r5, r5, 360                            # | Get MultipliedSeed % 360
+  sub r6, r6, r5                               # /
+  addi r6, r6, 1                               # Add 1 to final angle.
+exit:
+  mr r4, r29                                   # \
+  mr r5, r27                                   # / Restore modified register values.
 }
+
+
